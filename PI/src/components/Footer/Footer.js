3d724ef08a055eca6e43385526c9bcd98a1cd75e
cherreraposse-art <cherreraposse@udesa.edu.ar>
2026-03-29 22:41:05 -0300

    footer

---
+++ PI/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Integrantes: Justina Bazterrica - Catalina - Camila Herrera Posse</p>
+    </footer>
+  );
+}
+
+export default Footer;
