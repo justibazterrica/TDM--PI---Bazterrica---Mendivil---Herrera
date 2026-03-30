ea70986d6d85672ca2559fc22100c9400fdfef52
catalinamendivil <cmendivil@udesa.edu.ar>
2026-03-30 16:22:47 -0300

    agreguge nodemodules

--- PI/src/App.js
+++ PI/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import React from 'react';
+import Home from './screens/Home';
+import NotFound from "./screens/NotFound/Notfound";
+import {Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+
+      <Switch>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="*" component={NotFound}/>
+      
+      </Switch>
+
+      <Footer/> 
+    </React.Fragment>
   );
 }
 
