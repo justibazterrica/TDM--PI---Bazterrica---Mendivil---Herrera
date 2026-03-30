495b5d454f5c418614ba1028d34c31a99d9a27f1
cherreraposse-art <cherreraposse@udesa.edu.ar>
2026-03-29 23:09:54 -0300

    header

---
+++ PI/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+function Header() {
+  return (
+    <header>
+      <h1>Series</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+
+          <li>
+            <a href="/login">Login</a>
+          </li>
+
+          <li>
+            <a href="/register">Crear Cuenta</a>
+          </li>
+
+          <li>
+            <a href="/favorites">Favoritos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
