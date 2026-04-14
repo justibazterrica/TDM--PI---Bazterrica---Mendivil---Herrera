81c5772bc77b4c6514d6209142a65c47869f5a2e
justibazterrica <jbazterrica@udesa.edu.ar>
2026-04-14 09:46:38 -0300

    hice lo de las cookies de favoritos

--- PI/src/components/Pelicula/Pelicula.js
+++ PI/src/components/Pelicula/Pelicula.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import BotonFav from '../BotonFav/BotonFav'
 import Cookies from 'universal-cookie'
+import {Link} from 'react-router-dom'
 
-const Cookies = new Cookies();
+const cookies = new Cookies();
 
 export default function Pelicula (props) {
 
@@ -18,7 +19,7 @@ export default function Pelicula (props) {
                     <p><strong>Descripcion:</strong></p>
                     <p className="description">{props.sinopsis}</p>
                     <p className="mt-0 mb-0" id="genre"><strong>Genero: </strong>{props.genero} </p>
-                    { usuario ? <BotonFav id={props.id} /> : null }
+                    { usuario ? <BotonFav id={props.id} /> : <p className = "texto-alternativo"><Link to="/crear">Crear cuenta</Link> o <Link to="/login">iniciar sesion</Link> para agregar a favoritos</p> }
                 </section>
                 <img className="img" src={`https://image.tmdb.org/t/p/w500/${props.img}`} alt={props.name} />
             </section>
