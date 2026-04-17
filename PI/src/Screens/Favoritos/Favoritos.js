683843c381b3089b5f78af3003134b84aa09742b
cherreraposse-art <cherreraposse@udesa.edu.ar>
2026-04-17 14:50:49 -0300

    edite favoritos

--- PI/src/Screens/Favoritos/Favoritos.js
+++ PI/src/Screens/Favoritos/Favoritos.js
@@ -7,33 +7,77 @@ class Favoritos extends Component {
 
     this.state = {
       peliculasFavoritas: [],
-      seriesFavoritas: []
+      seriesFavoritas: [],
     };
   }
 
-  eliminarPelicula(id) {
-    let nuevasPeliculas = this.state.peliculasFavoritas.filter(function(pelicula) {
-      return pelicula.id !== id;
-    });
+  componentDidMount() {
+    let storagePeliculas = localStorage.getItem("favoritosPeliculas");
+    let storageSeries = localStorage.getItem("favoritosSeries");
 
-    this.setState({
-      peliculasFavoritas: nuevasPeliculas
+    let peliculasIds = storagePeliculas ? JSON.parse(storagePeliculas) : [];
+    let seriesIds = storageSeries ? JSON.parse(storageSeries) : [];
+
+    let peliculasRecuperadas = [];
+    let seriesRecuperadas = [];
+
+      peliculasIds.map(id => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c&language=es-ES`)
+          .then(response => response.json())
+          .then(data => {
+            peliculasRecuperadas.push(data);
+
+            this.setState({
+              peliculasFavoritas: peliculasRecuperadas
+            });
+          })
+          .catch(error => console.log(error));
+      });
+
+      seriesIds.map(id => {
+        fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c&language=es-ES`)
+          .then(response => response.json())
+          .then(data => {
+            seriesRecuperadas.push(data);
+
+            this.setState({
+              seriesFavoritas: seriesRecuperadas
+            });
+          })
+          .catch(error => console.log(error));
+      });
+    }
+
+  eliminarPelicula(id) {
+    let nuevasPeliculas = this.state.peliculasFavoritas.filter(pelicula=> pelicula.id !== id);
+
+    let storage = JSON.parse(localStorage.getItem("favoritosPeliculas"));
+    let nuevosIds = storage.filter(fav => fav !== id);
+
+    localStorage.setItem("favoritosPeliculas", JSON.stringify(nuevosIds));
+
+    this.setState({ 
+      peliculasFavoritas: nuevasPeliculas 
     });
   }
 
   eliminarSerie(id) {
-    let nuevasSeries = this.state.seriesFavoritas.filter(function(serie) {
-      return serie.id !== id;
-    });
+    let nuevasSeries = this.state.seriesFavoritas.filter(s => s.id !== id);
+
+    let storage = JSON.parse(localStorage.getItem("favoritosSeries"));
+    let nuevosIds = storage.filter(fav => fav !== id);
+
+    localStorage.setItem("favoritosSeries", JSON.stringify(nuevosIds));
 
     this.setState({
-      seriesFavoritas: nuevasSeries
-    });
+       seriesFavoritas: nuevasSeries 
+      });
   }
 
   render() {
 
   let contenidoPeliculas;
+  let contenidoSeries;
 
   if (this.state.peliculasFavoritas.length === 0) {
     contenidoPeliculas = <p>No hay películas favoritas</p>;
@@ -42,7 +86,9 @@ class Favoritos extends Component {
       <ul>
         {this.state.peliculasFavoritas.map((pelicula, idx) => (
           <li key={idx}>
-            <Link to="/detalle">{pelicula.titulo}</Link>
+            <Link to={`/Detalle/pelicula/${pelicula.id}`}>
+              {pelicula.title}
+            </Link>
 
             <button onClick={() => this.eliminarPelicula(pelicula.id)}>
               Eliminar
@@ -50,19 +96,19 @@ class Favoritos extends Component {
           </li>
         ))}
       </ul>
-    );
-  }
+     );
+   }
 
-  let contenidoSeries;
-
-  if (this.state.seriesFavoritas.length === 0) {
+   if (this.state.seriesFavoritas.length === 0) {
     contenidoSeries = <p>No hay series favoritas</p>;
   } else {
     contenidoSeries = (
       <ul>
         {this.state.seriesFavoritas.map((serie, idx) => (
           <li key={idx}>
-            <Link to="/detalle">{serie.titulo}</Link>
+            <Link to={`/Detalle/serie/${serie.id}`}>
+              {serie.name}
+          </Link>
 
             <button onClick={() => this.eliminarSerie(serie.id)}>
               Eliminar
@@ -87,8 +133,9 @@ class Favoritos extends Component {
         {contenidoSeries}
       </section>
     </main>
-  );
-}
+    );
+  }
 }
 
 export default Favoritos;
+
