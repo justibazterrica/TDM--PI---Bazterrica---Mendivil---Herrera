828d22580cb867dc328004a95cca09b624959ec0
justibazterrica <jbazterrica@udesa.edu.ar>
2026-05-11 17:28:22 -0300

    puse hooks

--- PI/src/components/DetalleSerie/DetalleSerie.js
+++ PI/src/components/DetalleSerie/DetalleSerie.js
@@ -1,56 +1,37 @@
-import React, { Component } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './DetalleSerie.css'
 import Loader from '../Loader/Loader'
 import Serie from '../Serie/Serie'
 
+function DetalleSerie( props) {
+    const [series, setSeries] = useState(null)
+
+    useEffect(() => { 
+        const id = props.match.params.id
+
+        fetch (`https://api.themoviedb.org/3/tv/${id}?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c&language=es-ES`)
+        .then ( response => response.json ())
+        .then ( data => setSeries (data))
+        .catch ( error => console.log(error))
+    }, [])
 
 
-class DetalleSerie extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            series: null
-        }
-    }
-
-    componentDidMount() {
-        
-        const id = this.props.match.params.id
-        
-        fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c&language=es-ES`)
-        .then(response => response.json())
-        .then(data => {
-            this.setState({ series: data })
-        })
-        .catch(error => console.log(error))
-        
-
-    }
-
-
-    render() {
-        const { series } = this.state
-        if (!series) {
-            return <Loader/>
-        }
-        return (
-            <div>
-                <Serie
-                    key = {series.id}
-                    id={series.id}
-                    name={series.name}
-                    calificacion={series.vote_average}
-                    estreno={series.first_air_date}
-                    sinopsis={series.overview}
-                    genero={series.genres ? series.genres.map(g => g.name).join(", ") : "Sin género"}
-                    img={series.poster_path}
-                />
-                
-            </div>
-        )
-        
-    }
-    
+return (
+    !series ?
+        <Loader /> :
+        <div>
+            <Serie
+                key={series.id}
+                id={series.id}
+                name={series.name}
+                calificacion={series.vote_average}
+                estreno={series.first_air_date}
+                sinopsis={series.overview}
+                genero={series.genres ? series.genres.map(g => g.name).join(", ") : "Sin género"}
+                img={series.poster_path}
+            />
+            
+        </div>
+)
 }
-
 export default DetalleSerie
