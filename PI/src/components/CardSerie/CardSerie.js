f95adef9e665cdeae5c141bc435d12dab7a4f431
justibazterrica <jbazterrica@udesa.edu.ar>
2026-05-12 13:26:56 -0300

    cambio a hooks

--- PI/src/components/CardSerie/CardSerie.js
+++ PI/src/components/CardSerie/CardSerie.js
@@ -48,3 +48,37 @@ export default SerieCard;
 
     
 
+import { useState, useEffect, useRef } from 'react'
+
+function SerieCard (props) {
+  const [ verMenos, setverMenos] = useState (false);
+  
+ function clickVerMenos (){
+    setverMenos(!verMenos)
+  }
+
+   return(
+
+            <article className='character-card'>
+
+                <img src={"https://image.tmdb.org/t/p/w342/" + props.img} alt={props.name}  className = "fotos" />
+                <h2 className = "titulo">{props.name} </h2> 
+
+
+               <section className="info">
+            
+                  <p className= {'extra-info ' + (verMenos ? 'false' : 'true') }>  {props.overview} </p>
+          
+              </section>
+
+              <Link to={`/Detalle/serie/${props.id}`} className="link"> Ir a detalle</Link>
+
+		        <button className="ver-mas" onClick={() => clickVerMenos()} >
+                  {verMenos ? "Ver más" : "Ver menos"}
+                </button>
+                
+        </article>
+
+        );
+
+}
