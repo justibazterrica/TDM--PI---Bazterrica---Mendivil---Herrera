41c0621c6fc49906bc8893b3c400654cb9f4bbdb
cherreraposse-art <cherreraposse@udesa.edu.ar>
2026-05-14 17:08:16 -0300

    cambio de componentes a hooks

--- PI/src/components/PaginaSeries/PaginaSerie.js
+++ PI/src/components/PaginaSeries/PaginaSerie.js
@@ -1,60 +1,49 @@
-import React, { Component } from 'react'
+import { useState, useEffect } from "react";
 import "./PaginaSerie.css"
 import CardSerie from "../CardSerie/CardSerie"
 import Loader from '../Loader/Loader'
 
-class PaginaSerie extends Component {
+function PaginaSerie() {
 
-    constructor(props) {
-        super(props)
-        this.state = { 
-            datos: [],
-            paginaSiguiente: 1,
-            valorFiltro: "" 
-        }
-    }
+    const [datos, setDatos] = useState([]);
+    const [paginaSiguiente, setPaginaSiguiente] = useState(1);
+    const [valorFiltro, setValorFiltro] = useState("");
 
-    componentDidMount() {
-        this.cargarMas();
-    }
+    useEffect(() => {
+        cargarMas();
+    }, []);
 
-    cargarMas() { 
-        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c&page=${this.state.paginaSiguiente}`)
+    function cargarMas() { 
+        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c&page=${paginaSiguiente}`)
             .then(response => response.json())
             .then(data => {
-                this.setState({ 
-                    datos: this.state.datos.concat(data.results),
-                    paginaSiguiente: this.state.paginaSiguiente + 1
-                })
+                setDatos(datos.concat(data.results));
+                setPaginaSiguiente(paginaSiguiente + 1);
             })
             .catch(error => console.log(error))
+    } 
+
+    function controlarCambios(evento) {
+        setValorFiltro(evento.target.value);
     }
 
-        controlarCambios(evento) {
-        this.setState({
-            valorFiltro: evento.target.value
-        });
-    }
-
-    render() {
-        let seriesFiltradas = this.state.datos.filter(serie => 
-            serie.name.toLowerCase().includes(this.state.valorFiltro.toLowerCase())
-        );
+     let seriesFiltradas = datos.filter(serie => serie.name.toLowerCase().includes(valorFiltro.toLowerCase())
+    );
 
         return (
             <div className="paginaSeries">
                 
                 <form onSubmit={(e) => e.preventDefault()} className="form-filtro">
                     <input 
-                        type="text" 
-                        placeholder="Filtrar series..." 
-                        onChange={(e) => this.controlarCambios(e)}
-                        value={this.state.valorFiltro}
+                    type="text"
+                    placeholder="Filtrar series..."
+                    onChange={controlarCambios}
+                    value={valorFiltro}
                     />
                 </form>
 
                 <section className="Series">
-                    {this.state.datos.length === 0 ? (
+                    {datos.length === 0 ? (
                         <Loader />
                     ) : (
                         seriesFiltradas.length > 0 ? (
@@ -67,18 +56,17 @@ class PaginaSerie extends Component {
                                     overview={serie.overview}
                                 />
                             ))
-                        ) : (
+                        ) : ( 
                             <h3>No se encontraron series con ese nombre</h3>
                         )
                     )}
                 </section>
                 
-                <button className="verMas" onClick={() => this.cargarMas()}>
+                <button className="verMas" onClick={cargarMas}>
                     Más series
                 </button>
             </div>
-        )
+        );
     }
-}
 
 export default PaginaSerie;
--- PI/src/components/ResultadosBusqueda/ResultadosBusqueda.js
+++ PI/src/components/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,64 +1,58 @@
-import React, { Component } from 'react'
-import CardPelicula from "../CardPelicula/CardPelicula"
-import CardSerie from "../CardSerie/CardSerie"
-import Loader from "../Loader/Loader"
+import { useState, useEffect } from "react";
+import CardPelicula from "../CardPelicula/CardPelicula";
+import CardSerie from "../CardSerie/CardSerie";
+import Loader from "../Loader/Loader";
 
-export default class ResultadosBusqueda extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            resultados: [],
-            tipo: this.props.tipo
-        }
-    }
+function ResultadosBusqueda(props) {
 
-    componentDidMount() {
-        console.log(this.props.tipo);
-        
-        fetch(`https://api.themoviedb.org/3/search/${this.props.tipo}?query=${this.props.search}&api_key=7af9e68f00d96b306cc0ab2e52ceaf9c`)
-        .then(response => response.json())
-        .then(data => {this.setState({ resultados: data.results}) })
-        .catch(error => console.log(error))
-    }
+    const [resultados, setResultados] = useState([]);
+    const [tipo, setTipo] = useState(props.tipo);
 
-  render() {
-    console.log(this.state.resultados);
-    console.log(this.props);
-    
+    useEffect(() => {
+
+        console.log(props.tipo);
+
+        fetch(`https://api.themoviedb.org/3/search/${props.tipo}?query=${props.search}&api_key=7af9e68f00d96b306cc0ab2e52ceaf9c`)
+            .then(response => response.json())
+            .then(data => setResultados(data.results))
+            .catch(error => console.log(error));
+
+    }, []);
+
+    console.log(resultados);
+    console.log(props);
+
     return (
-
         <div>
-        {this.state.tipo === "movie"? (<section className ="seccion" >
-                    {this.state.resultados.length === 0 ? <Loader/> : 
-                            
-                    this.state.resultados.map((pelicula, idx) => (
-                                <CardPelicula
-                                
-                                key = {idx}
+            {tipo === "movie" ? (
+                <section className="seccion">
+                    {resultados.length === 0 ? <Loader /> : resultados.map((pelicula, idx) => (
+                            <CardPelicula
+                                key={idx}
                                 id={pelicula.id}
-                                img = {pelicula.poster_path}
-                                title = {pelicula.original_title} 
-                                overview = {pelicula.overview}
-                                
-                                /> ))}
-            </section>) : (<section className="seccion" >
-                {this.state.resultados.length === 0 ? <Loader/> : 
-                        this.state.resultados.map((serie, idx) => (
-
-                            <CardSerie 
-                            
-                            key = {idx}
-                            id={serie.id}
-                            img = {serie.poster_path}
-                            name = {serie.original_name} 
-                            overview = {serie.overview}
-                            
-                            /> ))}
-            </section>) }
-
+                                img={pelicula.poster_path}
+                                title={pelicula.original_title}
+                                overview={pelicula.overview}
+                            />
+                        ))
+                    }
+                </section>
+            ) : (<section className="seccion">
+                    {resultados.length === 0 ? <Loader /> : resultados.map((serie, idx) => (
+                            <CardSerie
+                                key={idx}
+                                id={serie.id}
+                                img={serie.poster_path}
+                                name={serie.original_name}
+                                overview={serie.overview}
+                            />
+                        ))
+                    }
+                </section>
+            )}
         </div>
-    )
-  }
+    );
 }
 
+export default ResultadosBusqueda;
 
