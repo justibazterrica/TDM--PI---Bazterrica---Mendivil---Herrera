1a990fdeef23ea44b3801204d4ade2a8405994a5
justibazterrica <jbazterrica@udesa.edu.ar>
2026-05-14 16:28:40 -0300

    hooks

--- PI/src/components/CardSerie/CardSerie.js
+++ PI/src/components/CardSerie/CardSerie.js
@@ -1,59 +1,11 @@
-import React, { Component } from "react";
 import "./CardSerie.css"
 import {Link} from "react-router-dom";
-
-class SerieCard  extends Component {
-    constructor(props) {
-        super(props);
-         this.state = {
-      verMenos: false
-    };
-    }       
-
-    clickVerMenos() {
-    this.setState({
-      verMenos: !this.state.verMenos
-    });
-  }
-    render (){  
-      
-        console.log(this.props);
-        
-        return(
-
-            <article className='character-card'>
-
-                <img src={"https://image.tmdb.org/t/p/w342/" + this.props.img} alt={this.props.name}  className = "fotos" />
-                <h2 className = "titulo">{this.props.name} </h2> 
-
-
-               <section className="info">
-            
-                  <p className= {'extra-info ' + (this.state.verMenos ? 'false' : 'true') }>  {this.props.overview} </p>
-          
-              </section>
-
-              <Link to={`/Detalle/serie/${this.props.id}`} className="link"> Ir a detalle</Link>
-
-		        <button className="ver-mas" onClick={() => this.clickVerMenos()} >
-                  {this.state.verMenos ? "Ver más" : "Ver menos"}
-                </button>
-                
-        </article>
-
-        );
-
-    }}
-export default SerieCard;
-
-    
-
 import { useState, useEffect, useRef } from 'react'
 
 function SerieCard (props) {
   const [ verMenos, setverMenos] = useState (false);
   
- function clickVerMenos (){
+  function clickVerMenos (){
     setverMenos(!verMenos)
   }
 
@@ -82,3 +34,5 @@ function SerieCard (props) {
         );
 
 }
+
+export default SerieCard
